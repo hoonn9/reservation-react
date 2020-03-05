754f44afeaff39141e2c27d03f9c197c6df953e4
HXXNS <39939869+HXXNS@users.noreply.github.com>
2020-03-05 17:33:50 +0900

    #8

--- src/api/Type/searchType/searchType.js
+++ src/api/Type/searchType/searchType.js
@@ -3,16 +3,27 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     searchType: async (_, args) => {
+      const typeList = await prisma.types();
       const { checkIn, checkOut } = args;
-      //   const typeList = await prisma.types();
-      //   console.log(typeList);
-      //   const reservations = await prisma
-      //     .type({ id: typeList[0].id })
-      //     .reservations({
-      //       where: { checkIn_gt: checkIn, checkOut_lt: checkOut }
-      //     });
-
-      return prisma.types();
+      const promises = typeList.map(async type => {
+        const { typeCount, id: typeId } = type;
+        let reservations = await prisma.type({ id: typeId }).reservations({
+          where: { checkIn_gt: checkIn, checkOut_lt: checkOut }
+        });
+        if (reservations.length < typeCount) {
+          return type;
+        } else {
+          return {
+            updatedAt: "",
+            price: 0,
+            typeCount: 0,
+            typeName: "",
+            id: "",
+            createdAt: ""
+          };
+        }
+      });
+      return await Promise.all(promises);
     }
   }
 };

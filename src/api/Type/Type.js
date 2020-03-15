a101dea89d3f2e68e6eafb9192355b5ca629ee1d
HXXNS <39939869+HXXNS@users.noreply.github.com>
2020-03-16 00:00:11 +0900

    #25

--- src/api/Type/Type.js
+++ src/api/Type/Type.js
@@ -3,6 +3,7 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Type: {
     reservations: ({ id }) => prisma.type({ id }).reservations(),
-    subTypes: ({ id }) => prisma.type({ id }).subTypes()
+    subTypes: ({ id }) => prisma.type({ id }).subTypes(),
+    files: ({ id }) => prisma.type({ id }).files()
   }
 };

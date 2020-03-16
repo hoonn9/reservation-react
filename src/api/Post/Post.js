ba320cdb721602f13c0b3935fa274ff6f1e22101
HXXNS <39939869+HXXNS@users.noreply.github.com>
2020-03-17 00:27:01 +0900

    #13
    Fix Board datamodel

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,6 +2,7 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Post: {
-    user: ({ id }) => prisma.post({ id }).user()
+    user: ({ id }) => prisma.post({ id }).user(),
+    board: ({ id }) => prisma.post({ id }).board()
   }
 };

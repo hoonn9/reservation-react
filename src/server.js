d42f2d6cc34c9437a843725c0d52bbe8f40b2e1a
HXXNS <39939869+HXXNS@users.noreply.github.com>
2020-04-15 02:32:51 +0900

    #23

--- src/server.js
+++ src/server.js
@@ -5,7 +5,18 @@ import { authenticateJwt } from "./passport";
 import schema from "./schema";
 import { isAuthenticated } from "./middlewares";
 import { uploadController } from "./upload";
+
 const express = require("express");
+// const https = require("https");
+// const http = require("http");
+// const fs = require("fs");
+
+// const options = {
+// ca: fs.readFileSync("/etc/letsencrypt/live/hxxnsreservationc.tk/fullchain.pem"),
+// key: fs.readFileSync("/etc/letsencrypt/live/hxxnsreservationc.tk/privkey.pem"),
+// cert: fs.readFileSync("/etc/letsencrypt/live/hxxnsreservationc.tk/cert.pem")
+// };
+
 const PORT = process.env.PORT || 4000;
 
 //context : resolver 끼리의 공유
@@ -28,3 +39,7 @@ server.express.post("/api/upload", uploadController);
 server.start({ port: PORT }, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
+
+// server.start({port: PORT, https: options}, () =>
+//   console.log(`Server running on http://localhost:${PORT}`)
+// );
